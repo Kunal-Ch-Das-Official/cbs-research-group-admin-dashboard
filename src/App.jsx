bda76a2edb217fcb4498d291d39ecf3ae4278389
Kunal Ch Das <kunalchandradasofficial@gmail.com>
2024-08-30 00:01:15 +0530

    Create utils/loading-spinner & utils/custom-alert

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import Sidebar from "./components/reuseable/sidebar-menu/sidebar/Sidebar";
-import ToggleMenu from "./components/reuseable/sidebar-menu/toggle-menu/ToggleMenu";
+import { RiDeleteBin2Fill } from "react-icons/ri";
+import CustomModel from "./utils/custom-models/CustomModel";
 
 function App() {
   return (
     <>
-      <Sidebar />
+      <CustomModel
+        buttonText={"Got It"}
+        showOrHide={"Hide"}
+        closeButton={"close"}
+        statusIcon={<RiDeleteBin2Fill className="text-red-500 text-5xl" />}
+        alertHead={"Are You Really Want To Delete"}
+        message1={"If You Delete You Will Never Get It Back"}
+        message2={""}
+        buttonColor={"bg-red-500 text-white"}
+      />
     </>
   );
 }
