55118ef55391bcc32bf31e0db115fcce5556466e
Kunal Ch Das <kunalchandradasofficial@gmail.com>
2024-09-10 21:53:36 +0530

    create dashboard ui

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,5 +1,11 @@
+import DashBoardBody from "../../components/single-use/dashboard-body/DashBoardBody";
+
 const Dashboard = () => {
-  return <div className="text-center">Hello world</div>;
+  return (
+    <div className="text-center pt-28">
+      <DashBoardBody />
+    </div>
+  );
 };
 
 export default Dashboard;
