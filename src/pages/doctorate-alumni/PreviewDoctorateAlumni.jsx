8c89de9914c8c75bb2d1de66a85ebc42dedac8f0
Kunal Ch Das <kunalchandradasofficial@gmail.com>
2024-09-16 21:37:06 +0530

    Optimize students preview page

--- src/pages/doctorate-alumni/PreviewDoctorateAlumni.jsx
+++ src/pages/doctorate-alumni/PreviewDoctorateAlumni.jsx
@@ -1,11 +1,9 @@
 import { useEffect, useState } from "react";
-import { FaGoogleScholar } from "react-icons/fa6";
-import { FaResearchgate } from "react-icons/fa";
-import { Link, useParams } from "react-router-dom";
-import { FaLongArrowAltLeft } from "react-icons/fa";
+import { useParams } from "react-router-dom";
 import LoadingSpinner from "../../utils/common-loading-spinner/LoadingSpinner";
 import axios from "../../../axios/axios";
 import envConfig from "../../../envConfig";
+import StudentPreview from "../../components/reuseable/student-preview/StudentPreview";
 const PreviewDoctorateAlumni = () => {
   const { id } = useParams();
   const [alumniInfo, setAlumniInfo] = useState(null);
@@ -31,109 +29,23 @@ const PreviewDoctorateAlumni = () => {
     <>
       {loading === true && <LoadingSpinner />}
       {alumniInfo ? (
-        <div className="text-center mt-28">
-          <div className="bg-white p-4">
-            <div className="border-1 shadow-lg shadow-gray-700 rounded-lg">
-              <div className="flex rounded-t-lg bg-gray-200 sm:px-2 w-full">
-                <div className="h-40 w-40 overflow-hidden sm:rounded-full sm:relative sm:p-0 top-10 left-5 p-3">
-                  <img
-                    src={alumniInfo.profilePicture}
-                    alt={alumniInfo.profilePicturePublicId}
-                  />
-                </div>
-
-                <div className="w-11/12 sm:text-center pl-5 mt-10 text-start">
-                  <p className="font-poppins font-bold text-heading sm:text-4xl text-2xl">
-                    {alumniInfo.alumniName}
-                  </p>
-                  <p className="text-heading mt-1">Doctorate Alumni</p>
-                  <p className="text-center inline-flex item-center">
-                    <a
-                      href={`https://${alumniInfo.googleScholarId}`}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FaGoogleScholar className="text-xl text-gray-500 mt-1 hover:text-blue-500 transform translate-y-1 hover:scale-110 cursor-pointer" />
-                    </a>
-                    <a
-                      href={alumniInfo.researchGateId}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FaResearchgate className="text-xl text-gray-500 mt-1 hover:text-orange-300 ml-2 transform translate-y-1 hover:scale-110 cursor-pointer" />
-                    </a>
-                  </p>
-                </div>
-              </div>
-
-              <div className="p-5">
-                <div className="flex flex-col sm:flex-row sm:mt-10">
-                  <div className="flex flex-col sm:w-1/3">
-                    <div className="py-3 sm:order-none order-3">
-                      <h2 className="text-lg font-poppins font-bold text-start ml-0 lg:ml-6">
-                        Contact Details
-                      </h2>
-                      <div className="border-2 w-32 border-top-color my-3 ml-0 lg:ml-6"></div>
-
-                      <div className="flex flex-col justify-start ml-0 mr-2 text-lg font-bold text-gray-500">
-                        <div className="flex items-start my-1">
-                          <div className="ml-0 lg:ml-6">
-                            {alumniInfo.emailId}
-                          </div>
-                        </div>
-                        <div className="flex items-start my-1">
-                          <div className="ml-0 lg:ml-6">
-                            {" "}
-                            {alumniInfo.phoneNumber}
-                          </div>
-                        </div>
-                        <div className="flex flex-col items-start my-1">
-                          <div className="ml-0 lg:ml-6">
-                            {" "}
-                            BSC: {alumniInfo.bscDoneFrom}
-                          </div>
-                        </div>
-                        <div className="flex flex-col items-start my-1">
-                          <div className="ml-0 lg:ml-6">
-                            {" "}
-                            MSC: {alumniInfo.mscDoneFrom}
-                          </div>
-                        </div>
-                        <div className="flex items-start my-1">
-                          <div className="ml-0 lg:ml-6">
-                            {" "}
-                            {alumniInfo.yearOfPassout}
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="flex flex-col sm:w-2/3 sm:-mt-10">
-                    <div className="py-3">
-                      <h2 className="text-lg font-poppins font-bold text-start">
-                        About Info
-                      </h2>
-                      <div className="border-2 w-24 border-top-color my-3"></div>
-                      <p className="text-start">{alumniInfo.details}</p>
-                      <div className="flex justify-start mt-4">
-                        <Link
-                          to={"/admin-panel/manage-doctorate-alumni"}
-                          className="text-gray-700 text-lg font-bold bg-yellow-500 px-6 py-1 rounded-lg inline-flex items-center hover:bg-yellow-600 transform translate-y-1 hover:scale-110 cursor-pointer"
-                        >
-                          <FaLongArrowAltLeft />
-                          <span className="ml-2"> Back</span>
-                        </Link>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <StudentPreview
+          studentName={alumniInfo.alumniName}
+          profileImageUrl={alumniInfo.profilePicture}
+          previewHeading={"Doctorate Alumni"}
+          googleScholarId={alumniInfo.googleScholarId}
+          researchGateId={alumniInfo.researchGateId}
+          emailId={alumniInfo.emailId}
+          phoneNumber={alumniInfo.phoneNumber}
+          bscCollege={alumniInfo.bscDoneFrom}
+          mscCollege={alumniInfo.mscDoneFrom}
+          yearOfPassout={alumniInfo.yearOfPassout}
+          currentYear={null}
+          aboutInfo={alumniInfo.details}
+          goBackLink={"/admin-panel/manage-doctorate-alumni"}
+        />
       ) : (
-        <h1 className="text-gray-500 text-center text-2xl font-bold mt-24">
+        <h1 className="text-gray-500 text-center text-2xl font-bold pt-24">
           Alumni details are not available!
         </h1>
       )}
